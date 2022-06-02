8da863cc773f4bdb9551a1acf065b7509c2dea04
Fgarm <guilhermear.maturana@outlook.com>
2022-06-02 19:42:41 -0300

    Start of second task2

--- Modelo Relacional em SQL/hotel.sql
+++ Modelo Relacional em SQL/hotel.sql
@@ -1,1 +1,23 @@
 # disponível em https://github.com/Fgarm/database-1-project/
+
+#Entidades
+create table PREDIOS(
+ID integer auto_increment primary key,
+INAUGURACAO date,
+ULTIMO_LAUDO date
+);
+create table ANDARES(
+predio integer,
+numero integer,
+foreign key (predio) references PREDIO(ID),
+primary key(predio, numero)
+);
+create table QUARTOS(
+predio integer,
+andar integer,
+numero integer,
+foreign key (predio, andar) references ANDARES(predio, numero),
+primary key (predio, andar, numero)
+);
+#Relacionamentos N para N
+
