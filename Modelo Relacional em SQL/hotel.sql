5fa5757cdf99c9b8fcae74ffa530ae7361c0519e
Fgarm <guilhermear.maturana@outlook.com>
2022-06-06 17:18:21 -0300

    Conclusion of SQL code

--- Modelo Relacional em SQL/hotel.sql
+++ Modelo Relacional em SQL/hotel.sql
@@ -1,115 +1,12 @@
-# disponível em https://github.com/Fgarm/database-1-project/
-
-create table PREDIOS(
-ID integer auto_increment primary key,
-INAUGURACAO date,
-ULTIMO_LAUDO date
-);
-create table ANDARES(
-predio integer,
-numero integer,
-foreign key (predio) references PREDIO(ID) on delete cascade on update cascade,
-primary key(predio, numero)
-);
-
-create table CATEGORIAS(
-tipo varchar (20),
-diaria float,
-ocupantes_maximo integer
-);
-
-create table QUARTOS(
-predio integer,
-andar integer,
-numero integer,
-categoria varchar (20) not null,
-foreign key (categoria) references CATEGORIAS(tipo) on update cascade,
-foreign key (predio, andar) references ANDARES(predio, numero) on delete cascade on update cascade,
-primary key (predio, andar, numero)
-);
-
-create table MOBILIA(
-ID integer auto_increment primary key,
-vendedor varchar (30),
-aquisicao date,
-predio integer,
-andar integer,
-numero integer,
-foreign key (predio, andar, numero) references QUARTOS(predio, andar, numero)
-);
-
-create table MOVEIS(
-reclamacoes integer,
-ultima_manutencao date,
-tipo varchar(40),
-ID integer,
-foreign key (ID) references MOBILIA(ID)
-);
-
-create table ELETRODOMESTICOS(
-consumo_energetico float,
-durabilidade_estimada integer,
-modelo varchar(60),
-ID integer,
-foreign key (ID) references MOBILIA(ID)
-);
-
-create table PESSOAS(
-CPF char(12) primary key,
-nome varchar(40),
-telefone char(15)
-);
-
-create table CLIENTES(
-CPF char(12),
-foreign key (CPF) references PESSOAS(CPF),
-primary key (CPF),
-cadastro date
-);
-
-create table ESCOLARIDADES(
-nivel integer,
-descricao varchar(80) primary key
-);
-
-create table HORARIOS(
-tipo char(15) primary key,
-entrada integer,
-saida integer,
-horas_diarias integer
-);
-
 create table FUNCIONARIOS(
-CPF char(12),
-foreign key (CPF) references PESSOAS(CPF),
+id integer,
 salario float,
-id_contrato integer unique,
-horario char(15),
-foreign key (horario) references HORARIOS(horario)
-);
-
-create table FUNCIONARIO_FORMACAO(
-CPF char(12),
-foreign key (CPF) references FUNCIONARIOS(CPF),
-formacao varchar(80),
-foreign key (formacao) references ESCOLARIDADES(descricao),
-primary key (CPF, formacao)
+id_gerente integer,
+foreign key (id_gerente) references GERENTES(id)
 );
 
 create table GERENTES(
-CPF char(12),
-foreign key (CPF) references FUNCIONARIOS(CPF),
-primary key (CPF)
+id integer,
+foreign key (id) references FUNCIONARIOS(id),
+primary key (id)
 );
-
-create table ATENDENTES(
-CPF char(12),
-foreign key (CPF) references FUNCIONARIOS(CPF),
-avaliacao float,
-primary key (CPF)
-);
-
-
-
-
-
