e8081079c179f98c5ecde0f4cd7e2a9843b447ba
Fgarm <guilhermear.maturana@outlook.com>
2022-06-06 21:04:20 -0300

    Fixed: relashionship between manager and funcionarios now behaves like it should

--- Modelo Relacional em SQL/hotel.sql
+++ Modelo Relacional em SQL/hotel.sql
@@ -1,12 +1,76 @@
 # disponível em https://github.com/Fgarm/database-1-project/
-create database hotel;
-use hotel;
+
+drop database hotel_trabalho_guilherme;
+create database hotel_trabalho_guilherme;
+use hotel_trabalho_guilherme;
+
+create table PESSOAS(
+CPF char(12) primary key,
+nome varchar(40),
+telefone char(15)
+);
+
+create table CLIENTES(
+CPF char(12),
+foreign key (CPF) references PESSOAS(CPF) on update cascade on delete cascade,
+primary key (CPF),
+cadastro date
+);
+
+create table ESCOLARIDADES(
+nivel integer,
+descricao varchar(80) primary key
+);
+
+create table HORARIOS(
+tipo char(15),
+primary key (tipo),
+entrada integer,
+saida integer,
+horas_diarias integer
+);
+
+create table FUNCIONARIOS(
+CPF char(12),
+foreign key (CPF) references PESSOAS(CPF) on update cascade on delete cascade,
+id integer,
+salario float,
+id_contrato integer unique,
+horario char(15),
+foreign key (horario) references HORARIOS(tipo)
+);
+
+create table FUNCIONARIO_FORMACAO(
+CPF char(12),
+foreign key (CPF) references FUNCIONARIOS(CPF),
+formacao varchar(80),
+foreign key (formacao) references ESCOLARIDADES(descricao),
+primary key (CPF, formacao)
+);
+
+create table GERENTES(
+CPF char(12),
+foreign key (CPF) references FUNCIONARIOS(CPF) on update cascade on delete cascade,
+primary key (CPF)
+);
+alter table FUNCIONARIOS
+ADD gerente char(12),
+ADD foreign key (gerente) references GERENTES(CPF);
+create table ATENDENTES(
+CPF char(12),
+foreign key (CPF) references FUNCIONARIOS(CPF) on update cascade on delete cascade,
+avaliacao float,
+primary key (CPF)
+);
+
 create table PREDIOS(
 ID integer auto_increment primary key,
 INAUGURACAO date,
 ULTIMO_LAUDO date,
-gerente char(12)
+gerente char(12) not null,
+foreign key (gerente) references GERENTES(CPF)
 );
+
 create table ANDARES(
 predio integer,
 numero integer,
@@ -57,69 +121,12 @@ ID integer,
 foreign key (ID) references MOBILIA(ID)
 );
 
-create table PESSOAS(
-CPF char(12) primary key,
-nome varchar(40),
-telefone char(15)
-);
-
-create table CLIENTES(
-CPF char(12),
-foreign key (CPF) references PESSOAS(CPF),
-primary key (CPF),
-cadastro date
-);
-
-create table ESCOLARIDADES(
-nivel integer,
-descricao varchar(80) primary key
-);
-
-create table HORARIOS(
-tipo char(15),
-primary key (tipo),
-entrada integer,
-saida integer,
-horas_diarias integer
-);
-
-create table FUNCIONARIOS(
-CPF char(12),
-foreign key (CPF) references PESSOAS(CPF),
-id integer,
-salario float,
-id_contrato integer unique,
-horario char(15),
-foreign key (horario) references HORARIOS(tipo)
-);
-
-create table FUNCIONARIO_FORMACAO(
-CPF char(12),
-foreign key (CPF) references FUNCIONARIOS(CPF),
-formacao varchar(80),
-foreign key (formacao) references ESCOLARIDADES(descricao),
-primary key (CPF, formacao)
-);
-
-create table GERENTES(
-CPF char(12),
-foreign key (CPF) references FUNCIONARIOS(CPF),
-primary key (CPF)
-);
-
-create table ATENDENTES(
-CPF char(12),
-foreign key (CPF) references FUNCIONARIOS(CPF),
-avaliacao float,
-primary key (CPF)
-);
-
 create table RESERVAS(
 ID integer auto_increment,
 primary key (ID),
-cliente char(12),
+cliente char(12) not null,
 foreign key (cliente) references CLIENTES(CPF),
-atendente char(12),
+atendente char(12) not null,
 foreign key (atendente) references ATENDENTES(CPF),
 entrada date,
 saida date,
